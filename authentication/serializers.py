70c792a94581f6ad02f4dbe33a3dea12fce575d1
Jeevesh <jeevesh.garg.min20@iitbhu.ac.in>
2021-08-02 12:58:10 +0530

    auth done

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -6,18 +6,24 @@ from django.contrib.auth.models import User
 class TokenSerializer(serializers.Serializer):
     token = serializers.CharField(max_length=500)
 
-
 class LoginSerializer(serializers.Serializer):
-    # TODO: Implement login functionality
-    pass
-
+    username = serializers.CharField(required=True, max_length=255, min_length=1)
+    password = serializers.CharField(required=True,max_length=255, min_length=1)
 
 class RegisterSerializer(serializers.Serializer):
-    # TODO: Implement register functionality
-    pass
-
+    name = serializers.CharField(required=True,max_length=150, min_length=1)
+    email = serializers.EmailField(required=True, max_length=255, min_length=1)
+    username = serializers.CharField(required=True, max_length=255, min_length=1)
+    password = serializers.CharField(required=True,max_length=255, min_length=1)
+    def validate(self, data):
+        user = User.objects.filter(username=data['username'], email=data['email'])
+        if user:
+            raise serializers.ValidationError("Account already exists, please login")
+        else:
+            return data    
 
 class UserSerializer(serializers.ModelSerializer):
-    # TODO: Implement the functionality to display user details
-    pass
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'email', 'username')
     
--- authentication/views.py
+++ authentication/views.py
@@ -1,42 +1,45 @@
+from django.http import response
 from rest_framework import permissions
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from .serializers import (
-    LoginSerializer, RegisterSerializer, UserSerializer, TokenSerializer)
+from django.contrib.auth.models import User
+from .serializers import ( LoginSerializer, RegisterSerializer, UserSerializer, TokenSerializer)
+from django.contrib.auth  import authenticate,
 
 
 def create_auth_token(user):
-    """
-    Returns the token required for authentication for a user.
-    """
     token, _ = Token.objects.get_or_create(user=user)
     return token
 
-
 class LoginView(generics.GenericAPIView):
-    """
-    TODO:
-    Implement login functionality, taking username and password
-    as input, and returning the Token.
-    """
-    pass
-
-
+    permission_classes = (permissions.AllowAny,)
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        user=authenticate(username=serializer.data['username'], password=serializer.data['password'])
+        if user is not None:
+            token=create_auth_token(serializer.validated_data['username'])
+            return Response({'token':token}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 class RegisterView(generics.GenericAPIView):
-    """
-    TODO:
-    Implement register functionality, registering the user by
-    taking his details, and returning the Token.
-    """
-    pass
-
+    permission_classes = (permissions.AllowAny,)
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.create_user(username=serializer.validated_data['username'], password=serializer.validated_data['password'])
+            user.email =serializer.validated_data['email']
+            user.name =serializer.validated_data['name']
+            user.save()
+            token=create_auth_token(serializer.validated_data['username'])
+            return Response({'token':token}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
 class UserProfileView(generics.RetrieveAPIView):
-    """
-    TODO:
-    Implement the functionality to retrieve the details
-    of the logged in user.
-    """
-    pass
+    permission_classes = (permissions.IsAuthenticated,)
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data,status=status.HTTP_200_OK)
