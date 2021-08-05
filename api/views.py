a7d35112191057aded8a9b9b6faaeb3fefc0b4fb
Naveen Kumar <naveenkumar12062002@gmail.com>
2021-08-05 11:13:14 +0530

    Added Comments

--- api/views.py
+++ api/views.py
@@ -2,16 +2,17 @@ from django.http.response import HttpResponse
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import status
-from rest_framework import mixins
-from rest_framework.response import Response
 from .serializers import *
 from .models import Todo
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from rest_framework import mixins
+from rest_framework.response import Response
 
 
 # class for the cloolabartor view 
 class TodoAddCollaboratorsView(generics.GenericAPIView):
+    # check for the authenticity 
     permission_classes = (permissions.IsAuthenticated, )
     serializer_class = TodoCollaboratorsSerializer
 
@@ -22,24 +23,34 @@ class TodoAddCollaboratorsView(generics.GenericAPIView):
     def post(self, request, id):
         serializer = TodoCollaboratorsSerializer(data=request.data)
 
+
         # checking the validity of the serializer
         if serializer.is_valid(raise_exception=True):     
             usernames = request.data['usernames'].replace(' ','').split(',')
+            # setting and getitng the list of users 
             usernames = set(usernames)
+
+
             usernames = list(usernames)
+
+
 
 
 
             try:
                 todo = Todo.objects.get(id=id)
+                # getting the ids 
             except:
+                # returning with the error code of 404 
                 return Response({ "todo": "Not able to found" }, status=404)
 
             # checking the equality ( same )
             if request.user == todo.creator:
+
                 all_users = []
                 for user in User.objects.all():
                     all_users.append(user.username)
+                    
                 response = {
                     "successful": [], "alreadyExist": [], "incorrectUsername":[]
                 }
@@ -48,10 +59,13 @@ class TodoAddCollaboratorsView(generics.GenericAPIView):
                 # finding user in all the users 
                 for user in usernames:
                     if user in todo.collaborators:
+                        # telling the already existing 
                         response['alreadyExist'].append(user)
                     elif user not in all_users:
+                        # returning incorrect username 
                         response['incorrectUsername'].append(user)
                     else:
+                        # returning successful 
                         response['successful'].append(user)
                         todo.collaborators.append(user)
                         todo.save()
@@ -60,8 +74,12 @@ class TodoAddCollaboratorsView(generics.GenericAPIView):
                 # checking which exist means either it is successful, alreadyExists or incorrectUsername 
                 if not response['successful']:
                     del response['successful']
+
+
                 if not response['alreadyExist']:
                     del response['alreadyExist']
+
+
                 if not response['incorrectUsername']:
                     del response['incorrectUsername']
                 return Response(response, status=200)
@@ -90,12 +108,18 @@ class TodoRemoveCollaboratorsView(generics.GenericAPIView):
         serializer = TodoCollaboratorsSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):     
             usernames = request.data['usernames'].replace(' ','').split(',')
+            # same getting and setting the users 
             usernames = set(usernames)
             usernames = list(usernames)
+
+
             try:
                 todo = Todo.objects.get(id=id)
             except:
                 return Response({ "todo": "Not found" }, status=404)
+                # returning with the error code of 404
+
+
             if request.user == todo.creator:
                 all_users = []
                 for user in User.objects.all():
@@ -103,6 +127,8 @@ class TodoRemoveCollaboratorsView(generics.GenericAPIView):
                 response = {
                     "successful": [], "incorrectUsername":[]
                 }
+
+                # finding the user in users 
                 for user in usernames:
                     if user in todo.collaborators:
                         response['successful'].append(user)
@@ -110,6 +136,8 @@ class TodoRemoveCollaboratorsView(generics.GenericAPIView):
                         todo.save()
                     else:
                         response['incorrectUsername'].append(user)
+
+                        
                 if not response['successful']:
                     del response['successful']
                 if not response['incorrectUsername']:
