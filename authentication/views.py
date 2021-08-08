89d18cbb1e1f8b4404cb0d4a0f809e1a804cd775
atharva_bhatt <atharvabhatt200@gmail.com>
2021-08-08 21:21:46 +0530

    Completed all the tasks - Atharva Bhatt

--- authentication/views.py
+++ authentication/views.py
@@ -3,40 +3,53 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from .serializers import (
     LoginSerializer, RegisterSerializer, UserSerializer, TokenSerializer)
 
 
 def create_auth_token(user):
-    """
-    Returns the token required for authentication for a user.
-    """
     token, _ = Token.objects.get_or_create(user=user)
     return token
 
 
 class LoginView(generics.GenericAPIView):
-    """
-    TODO:
-    Implement login functionality, taking username and password
-    as input, and returning the Token.
-    """
-    pass
+    serializer_class = LoginSerializer
 
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        if user:
+            token = create_auth_token(user)
+            response_data = {"token": "Token " + str(token)}
+            return Response(response_data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 class RegisterView(generics.GenericAPIView):
-    """
-    TODO:
-    Implement register functionality, registering the user by
-    taking his details, and returning the Token.
-    """
-    pass
+    serializer_class = RegisterSerializer
 
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = create_auth_token(user)
+        response_data = {"token": "Token " + str(token)}
+        return Response(response_data, status=status.HTTP_200_OK)
 
 class UserProfileView(generics.RetrieveAPIView):
-    """
-    TODO:
-    Implement the functionality to retrieve the details
-    of the logged in user.
-    """
-    pass
+    permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = UserSerializer
+
+    def get(self, request):
+        serializer = self.get_serializer(request.user)
+        response_data = {
+            "id":  serializer.data['id'],
+            "name": serializer.data['first_name'] + " " + serializer.data['last_name'],
+            "email": serializer.data['email'],
+            "username": serializer.data['username']
+        }
+        
+        return Response(response_data, status=status.HTTP_200_OK)
